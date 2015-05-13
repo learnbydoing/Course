c0d1ce8e3c87f894534f0e1363578d899a13a383
Johnny <johnny@mbam-gpo.cstcis.cti.depaul.edu>
2015-05-13 17:53:51 -0500

    add new view controllers

---
+++ CSC471/Project/RZHUANG.TourItinerary/RZHUANG.TourItinerary/SearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RZHUANG.TourItinerary
+//
+//  Created by Johnny on 5/13/15.
+//  Copyright (c) 2015 CDM of DePaul University. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
