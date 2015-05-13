45e604d7b0051960bc0f447e176b74a006a17958
Johnny <johnny@mbam-gpo.cstcis.cti.depaul.edu>
2015-05-13 17:50:05 -0500

    initial

--- CSC471/Project/RZHUANG.TourItinerary/RZHUANG.TourItinerary/SettingsViewController.swift
+++ CSC471/Project/RZHUANG.TourItinerary/RZHUANG.TourItinerary/SettingsViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SecondViewController.swift
+//  SettingsViewController.swift
 //  RZHUANG.TourItinerary
 //
 //  Created by Johnny on 5/13/15.
@@ -8,18 +8,16 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SettingsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
-
