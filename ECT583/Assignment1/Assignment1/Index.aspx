f02a3b4e07d99b2aa7014065ff2867c94e8a7c6e
rzhuang <rzhuang@cdm.depaul.edu>
2016-03-29 17:40:35 -0500

    ECT583 assignment1

---
+++ ECT583/Assignment1/Assignment1/Index.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Index.aspx.cs" Inherits="Assignment1.Index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="mainBody">
+		<header>
+			<hgroup>
+				<h1>Welcome to this page</h1>
+			</hgroup>
+		</header>
+		<section>
+				Visisted Times: <asp:Label runat="server" ID="lblVisistedTimes"></asp:Label> <br/>
+				Message: <asp:Label runat="server" ID="lblMessage"></asp:Label> <br/>
+		</section>
+		<footer>
+			<p>Try to refresh this page to see different result.</p>
+		</footer>
+	</div>
+    </form>
+</body>
+</html>
---
+++ ECT583/Assignment1/Assignment1/Index.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Assignment1
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            Session["VisitedTimes"] = (int)Session["VisitedTimes"] + 1;
+            int visitedtimes = ((int)Session["VisitedTimes"]);
+            lblVisistedTimes.Text = visitedtimes.ToString();
+            if (visitedtimes == 1)
+            {
+                lblMessage.Text = "Welcome to the site!";
+            }
+            else if (visitedtimes >= 2 && visitedtimes <= 10)
+            {
+                lblMessage.Text = "Thanks for coming back!";
+
+            }
+            else if (visitedtimes >= 11 && visitedtimes <= 50)
+            {
+                lblMessage.Text = "Ummm… not much going on here, feel free to visit other sites.";
+            }
+            else if (visitedtimes >= 51 && visitedtimes <= 100)
+            {
+                lblMessage.Text = "Be patient..";
+            }
+            else // > 100
+            {
+                lblMessage.Text = "Leave me alone";
+            }            
+        }
+    }
+}
