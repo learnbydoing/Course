2930e87820c4a28997c4243031d8a4fea123af6b
Johnny <jojozhuang@gmail.com>
2016-04-09 14:33:25 -0500

    ect583 assignment2

---
+++ ECT583/Assignment2/Assignment2/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.States = State.List();
+            return View(new RegisterViewModel() { State = "AL" });
+        }
+
+        [HttpPost]
+        public ActionResult Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.States = State.List();
+                return View("Summary", reg);
+            }
+            else
+            {
+                ViewBag.States = State.List();                
+                return View(reg);
+            }
+        }
+
+        public ActionResult Summary(RegisterViewModel summary)
+        {
+            ViewBag.States = State.List();
+            return View(summary);
+        }
+    }
+}
---
+++ ECT583/Assignment2/Assignment2/Models/RegisterViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public class RegisterViewModel
+    {
+        public RegisterViewModel()
+        {
+            Gender = true;
+            Age = 18;
+        }
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be between {2} and {1} characters.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Gender")]
+        public bool Gender { get; set; }
+
+        [Display(Name = "Age")]
+        [Range(18, 100, ErrorMessage = "The {0} must be between {1} and {2}.")]
+        public int Age { get; set; }
+
+        [Required]
+        [Display(Name = "Address1")]
+        public string Address1 { get; set; }
+
+        [Display(Name = "Address2")]
+        public string Address2 { get; set; }
+
+        [Required]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required]
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Required]
+        [Display(Name = "Zip")]
+        public string Zip { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Would like to receive email about special offers")]
+        public bool Subscription { get; set; }
+    }
+    public class SummaryModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string Age { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Email { get; set; }
+        public string Subscription { get; set; }
+    }
+}
