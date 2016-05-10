67a976da50626dcae65f36b5093c0b612589a49e
Johnny <jojozhuang@gmail.com>
2016-05-10 07:41:44 -0500

    se452 homework3

---
+++ SE452/Homework3/hw3/src/java/OrderItem.java
@@ -0,0 +1,53 @@
+public class OrderItem {
+    private ProductItem item;
+    private int quantity;
+
+    public OrderItem(ProductItem item) {
+        setItem(item);
+        setQuantity(1);
+    }
+
+    public ProductItem getItem() {
+        return item;
+    }
+
+    protected void setItem(ProductItem item) {
+        this.item = item;
+    }
+
+    public String getItemId() {
+        return getItem().getId();
+    }
+    
+    public String getItemName() {
+        return getItem().getName();
+    }
+
+    public int getItemType() {
+        return getItem().getType();
+    }
+
+    public double getUnitPrice() {
+        return getItem().getDiscountedPrice();
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void incrementItemQuantity() {
+        setQuantity(getQuantity() + 1);
+    }
+
+    public void cancelOrder() {
+        setQuantity(0);
+    }
+
+    public double getTotalCost() {
+        return(getQuantity() * getUnitPrice());
+    }
+}
---
+++ SE452/Homework3/hw3/src/java/OrdersHashMap.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class OrdersHashMap {
+    public static HashMap<String, ArrayList<OrderItem>> orders = new HashMap<String, ArrayList<OrderItem>>();
+    public OrdersHashMap() {
+        //replaced with OrderList
+    }
+}
