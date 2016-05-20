d097da1a8b1a66cb2d9cbbb53c68ec1e1ac77a30
Johnny <jojozhuang@gmail.com>
2016-05-20 15:03:53 -0500

    csc452 final

---
+++ CSC452/Final/TestScript.sql
@@ -0,0 +1,29 @@
+2)
+EXEC update_expiration_date(90, '01-SEP-20');
+EXEC update_expiration_date(2009, '01-SEP-20');
+
+3)
+EXEC video_search('ocean');
+EXEC video_search('PRETTY WOMAN', 'Blu-Ray');
+EXEC video_search('Pretty Woman');
+EXEC video_search('Another');
+EXEC video_search('ANOTHER', 'BLU');
+EXEC video_search('Story');
+EXEC video_search('Story', 'TAPE');
+
+4)
+EXEC video_checkout(330, 6001, '18-APR-14');
+EXEC video_checkout(2009, 6015, '18-APR-16');
+
+5)
+EXEC video_return(123, '18-APR-16');
+EXEC video_return(6010, '18-APR-17');
+EXEC video_return(6013, '18-APR-17');
+EXEC video_return(6020, '01-MAY-16');
+
+6)
+EXEC print_unreturned_video(90);
+EXEC print_unreturned_video(2004);
+EXEC print_unreturned_video(2008);
+EXEC print_unreturned_video(2002);
+EXEC print_unreturned_video(2003);
